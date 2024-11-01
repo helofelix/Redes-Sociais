dcff553553aeb9ad14711e69eb7fbfd4fd9f9ed8
helofelix <107410996+helofelix@users.noreply.github.com>
2024-11-01 11:44:49 -0300

    Add files via upload

--- informaçõesGlobais.js
+++ informaçõesGlobais.js
@@ -3,6 +3,15 @@ const url = "https://raw.githubusercontent.com/SSguilhermeonrails/api/main/dados
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados);
+    console.log(dados.tempo_medio)
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem
+    ${dados.total_pessoas_mundo} de pessoas e que aproximadamente 
+    ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média
+    ${dados.tempo_medio} horas conectadas.`
+    
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
 visualizarInformacoesGlobais()
